dc261fa86d9451bffa747f8900ae32d0ee988e5d
Denys Baliuk <denuk14228@gmail.com>
2026-04-14 01:08:14 +0300

    second try after fixed project game-models

--- db/models.py
+++ db/models.py
@@ -18,9 +18,19 @@ class Guild(models.Model):
 
 
 class Player(models.Model):
-    nickname = models.CharField(max_length=255)
+    nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
-    rece = models.ForeignKey(Race, on_delete=models.CASCADE)
-    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
+
+    race = models.ForeignKey(
+        "Race",
+        on_delete=models.CASCADE
+    )
+
+    guild = models.ForeignKey(
+        "Guild",
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
     created_at = models.DateTimeField(auto_now_add=True)
