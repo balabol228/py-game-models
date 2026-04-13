6b0fc99816f08a2c5295de6d4a7326accdbcd2b1
Denys Baliuk <denuk14228@gmail.com>
2026-04-14 01:12:59 +0300

    4 try after fixed project game-models

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
+import json
 
 
 def main() -> None:
-    import json
 
     with open("players.json", "r", encoding="utf-8") as file:
         data = json.load(file)
 
     for player in data:
-        race_data = player["race"]
+
+        race_data = player.get("race")
 
         race, _ = Race.objects.get_or_create(
             name=race_data["name"],
-            defaults={
-                "description": race_data.get("description", "")
-            }
+            defaults={"description": race_data.get("description", "")}
         )
 
         guild_data = player.get("guild")
@@ -25,9 +24,7 @@ def main() -> None:
         if guild_data:
             guild, _ = Guild.objects.get_or_create(
                 name=guild_data["name"],
-                defaults={
-                    "description": guild_data.get("description", "")
-                }
+                defaults={"description": guild_data.get("description")}
             )
 
         Player.objects.create(
